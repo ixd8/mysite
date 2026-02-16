2849ef59542ad10a6eaa3ae6526ceb3094ae8f75
XD8 <tellmeArash@gmail.com>
2026-02-17 02:26:08 +0330

    ssa

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,3 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+    "/var/www/statics/",
+]
